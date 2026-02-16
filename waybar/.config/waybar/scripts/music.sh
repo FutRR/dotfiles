451665f3b994fc11a5a8f84d091db9a3f9bbdc23
FutRR <futterermaxime@gmail.com>
2026-02-16 17:35:57 +0100

    update music.sh to prioritize music over videos

--- waybar/.config/waybar/scripts/music.sh
+++ waybar/.config/waybar/scripts/music.sh
@@ -1,20 +1,30 @@
-#!/bin/bash
+#!/usr/bin/env bash
 
 playerctl=$(which playerctl 2>/dev/null || echo /usr/bin/playerctl)
-player=$($playerctl -l 2>/dev/null | head -n 1)
+
+players=$($playerctl -l 2>/dev/null)                            # list of players
+
+first=$(echo "$players" | head -n 1)                            # 1st player
+second=$(echo "$players" | head -n 2 | tail -n 1)               # 2nd player
+
+album=$($playerctl -p "$first" metadata album 2>/dev/null)      # value of album metadata for 1st player
+
+player=$([ -n "$album" ] && echo "$first" || echo "$second")    # if 1st player has no album (not music) then display the 2nd one
+                                                                # ex: display music over youtube video
+
 
 case "$1" in
     playpause)
-        $playerctl play-pause 2>/dev/null
+        $playerctl -p "$player" play-pause 2>/dev/null
         ;;
     next)
-        $playerctl next 2>/dev/null
+        $playerctl -p "$player" next 2>/dev/null
         ;;
     prev)
-        $playerctl previous 2>/dev/null
+        $playerctl -p "$player" previous 2>/dev/null
         ;;
     *)
-
+        
         if [ -n "$player" ]; then
             format=$($playerctl -p "$player" metadata --format "{{ artist }} - {{ title }}")
             status=$($playerctl -p "$player" status)
