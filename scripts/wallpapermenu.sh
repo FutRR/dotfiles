8077f02ddbb82a1a74be123a7ce29eb54b327b9a
FutRR <futterermaxime@gmail.com>
2026-02-17 23:09:28 +0100

    update wallpaper handling and simplify waybar reload script

--- scripts/wallpapermenu.sh
+++ scripts/wallpapermenu.sh
@@ -3,18 +3,22 @@
 FOLDER=~/Images/wallpapers/
 SCRIPT=~/dotfiles/scripts/waybar_reload.sh
 
+CACHE_FILE="/tmp/current_wallpaper.png"
+
 menu() {
     if CHOICE=$(nsxiv -to "$FOLDER"/*); then
         [ -n "$CHOICE" ] && wal -i "$CHOICE" -o "$SCRIPT"
+        cp "$CHOICE" "$CACHE_FILE"
     fi
 }
 
 case "$#" in
     0) menu;;
     1) if [ "$1" = "-r" ]; then
-           wal -i "$FOLDER" -o $SCRIPT
+            RANDOM_WALL=$(find "$FOLDER" -type f -print0 | shuf -z -n 1 | xargs -0 echo)
+            cp "$RANDOM_WALL" "$CACHE_FILE" && wal -i "$RANDOM_WALL" -o "$SCRIPT"
        else
-           wal -i "$1" -o $SCRIPT
+           wal -i "$1" -o "$SCRIPT"
        fi;;
     *) exit 0;;
 esac
--- scripts/waybar_reload.sh
+++ scripts/waybar_reload.sh
@@ -1,12 +1,4 @@
 #!/bin/bash
 
-CONFIG="$HOME/dotfiles/waybar/.config/waybar/config.jsonc"
-STYLE="$HOME/dotfiles/waybar/.config/waybar/style.css"
-
 killall waybar
-waybar &
-
-while inotifywait -e modify "$CONFIG" "$STYLE"; do
-    killall waybar
-    waybar  &
-done
+waybar  &
