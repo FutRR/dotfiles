515f3721bf7c458a460bd38ff249d1401cf40091
FutRR <futterermaxime@gmail.com>
2026-02-17 12:51:22 +0100

    simplified music script with loop in players

--- scripts/music.sh
+++ scripts/music.sh
@@ -1,55 +1,30 @@
 #!/usr/bin/env bash
 
-playerctl=$(which playerctl 2>/dev/null || echo /usr/bin/playerctl)
+PLAYERCTL=$(which playerctl 2>/dev/null || echo /usr/bin/playerctl)
 
-players=$($playerctl -l 2>/dev/null)                            # list of players
-
-first=$(echo "$players" | head -n 1)                            # 1st player
-second=$(echo "$players" | head -n 2 | tail -n 1)               # 2nd player
-
-album=$($playerctl -p "$first" metadata album 2>/dev/null)      # value of album metadata for 1st player
-
-player=$([ -n "$album" ] && echo "$first" || echo "$second")    # if 1st player has no album (not music) then display the 2nd one
-                                                                # ex: display music over youtube video
+PLAYERS=$($PLAYERCTL -l 2>/dev/null) # list of players
 
 
 case "$1" in
     playpause)
-        $playerctl -p "$player" play-pause 2>/dev/null
+        $PLAYERCTL play-pause 2>/dev/null
         ;;
     next)
-        $playerctl -p "$player" next 2>/dev/null
+        $PLAYERCTL next 2>/dev/null
         ;;
     prev)
-        $playerctl -p "$player" previous 2>/dev/null
+        $PLAYERCTL previous 2>/dev/null
         ;;
     *)
-        
-        if [ -n "$player" ]; then
-            format=$($playerctl -p "$player" metadata --format "{{ artist }} - {{ title }}")
-            status=$($playerctl -p "$player" status)
 
-            case "$status" in
-                Playing)
-                    icon=""
-                    ;;
-                Paused)
-                    icon=""
-                    ;;
-                Stopped)
-                    icon=""
-                    ;;
-                *)
-            esac
+        for PLAYER in $PLAYERS; do
+            [ "$(playerctl --player="$PLAYER" status 2>/dev/null)" != "Playing" ] && continue
+                ICON=""
+                echo "${ICON} $($PLAYERCTL -p "$PLAYER" metadata --format '{{ artist }} - {{ title }}')"
+        done
 
-            if [ -n "$status" ] && [ -n "$format" ]; then
-                echo "${icon} ${format}"
-            else
-                echo "En lecture"
-            fi
-        else
-            echo ""
-        fi
+        ICON=""
+        echo "${ICON} $($PLAYERCTL -p chromium metadata --format '{{ artist }} - {{ title }}')"
         ;;
 esac
 
