cece836163e630e27003323da1256f8a3a92bd0e
FutRR <futterermaxime@gmail.com>
2026-02-17 23:09:57 +0100

    refactor music script to improve player status handling and icon assignment

--- scripts/music.sh
+++ scripts/music.sh
@@ -18,12 +18,29 @@ case "$1" in
     *)
 
         for PLAYER in $PLAYERS; do
-            [ "$(playerctl --player="$PLAYER" status 2>/dev/null)" != "Playing" ] && continue
-                ICON=""
-                echo "${ICON} $($PLAYERCTL -p "$PLAYER" metadata --format '{{ artist }} - {{ title }}')"
+
+            STATUS=$($PLAYERCTL -p "$PLAYER" status 2>/dev/null)
+
+            case "$STATUS" in
+                Playing)
+                    ICON=""
+                    ;;
+                Paused)
+                    ICON=""
+                    ;;
+                *)
+                    ICON=""
+                    ;;
+            esac
+
+            if [ "$STATUS" != "Playing" ] || [ -z "$($PLAYERCTL -p "$PLAYER" metadata --format '{{ album }}' 2>/dev/null)" ]; then
+                echo "${ICON} $($PLAYERCTL -p "$PLAYER" metadata --format '{{ artist }} - {{ title }}')"    
+
+                [[ "$($PLAYERCTL -p "$PLAYER" metadata 2>/dev/null)" == *"No player could handle this command"* ]] && echo "" && exit 0
+            fi
+                
         done
 
-        ICON=""
         echo "${ICON} $($PLAYERCTL -p chromium metadata --format '{{ artist }} - {{ title }}')"
         ;;
 esac
