0d9c88546316816ca9feca72d96ecef6c9e44f0f
FutRR <futterermaxime@gmail.com>
2026-02-19 19:53:57 +0100

    refactor: streamline player status handling and improve output formatting in music script

--- scripts/music.sh
+++ scripts/music.sh
@@ -4,9 +4,6 @@ PLAYERCTL=$(which playerctl 2>/dev/null || echo /usr/bin/playerctl)
 
 PLAYERS=$($PLAYERCTL -l 2>/dev/null) # list of players
 
-
-
-
 case "$1" in
     playpause)
         $PLAYERCTL play-pause 2>/dev/null
@@ -18,36 +15,27 @@ case "$1" in
         $PLAYERCTL previous 2>/dev/null
         ;;
     *)
-
-        ( [[ ! $($PLAYERCTL -l | grep -q "instance") ]] && echo "" & exit 0 )
-
+        ACTIVE_PLAYER=""
+        PAUSED_PLAYER=""
         for PLAYER in $PLAYERS; do
 
             STATUS=$($PLAYERCTL -p "$PLAYER" status 2>/dev/null)
 
-            case "$STATUS" in
-                Playing)
-                    ICON=""
-                    ;;
-                Paused)
-                    ICON=""
-                    ;;
-                *)
-                    ICON=""
-                    ;;
-            esac
-
-            if [ "$STATUS" != "Playing" ] || [ -z "$($PLAYERCTL -p "$PLAYER" metadata --format '{{ album }}' 2>/dev/null)" ]; then
-                echo "${ICON} $($PLAYERCTL -p "$PLAYER" metadata --format '{{ artist }} - {{ title }}')"    
-
-                [[ "$($PLAYERCTL -p "$PLAYER" metadata 2>/dev/null)" == *"No player could handle this command"* ]] && echo "" && exit 0
+            if [[ "$STATUS" == "Playing" ]]; then
+                ACTIVE_PLAYER="$PLAYER"
+                break
+            elif [[ "$STATUS" == "Paused" && -z "$PAUSED_PLAYER" ]]; then
+                PAUSED_PLAYER="$PLAYER"
             fi
-                
+
         done
 
-        echo "${ICON} $($PLAYERCTL -p chromium metadata --format '{{ artist }} - {{ title }}')"
+        if [[ -n "$ACTIVE_PLAYER" ]]; then
+            echo " $($PLAYERCTL -p "$ACTIVE_PLAYER" metadata --format '{{ artist }} - {{ title }}')"
+        elif [[ -n "$PAUSED_PLAYER" ]]; then
+            echo " $($PLAYERCTL -p "$PAUSED_PLAYER" metadata --format '{{ artist }} - {{ title }}')"
+        else
+            echo ""
+        fi
         ;;
 esac
-
-
-    
